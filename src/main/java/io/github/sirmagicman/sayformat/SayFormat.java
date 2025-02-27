69dd27534c8281afa101c60a4674c610d570a242
Kyle Senerpida <46883293+kyleseven@users.noreply.github.com>
2025-02-27 05:02:32 -0600

    Remove unused listener

--- src/main/java/io/github/sirmagicman/sayformat/SayFormat.java
+++ src/main/java/io/github/sirmagicman/sayformat/SayFormat.java
@@ -3,7 +3,6 @@ package io.github.sirmagicman.sayformat;
 import co.aikar.commands.PaperCommandManager;
 import io.github.sirmagicman.sayformat.commands.MainCommand;
 import io.github.sirmagicman.sayformat.config.MainConfig;
-import io.github.sirmagicman.sayformat.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SayFormat extends JavaPlugin {
@@ -20,7 +19,6 @@ public final class SayFormat extends JavaPlugin {
         PaperCommandManager commandManager = new PaperCommandManager(this);
         commandManager.registerCommand(new MainCommand());
         MainConfig.getInstance();
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
     @Override
